ece81f6b82edb3bf0d886366e0297c47dd69cf88
anjanayekatyal-png <anjanaye.katyal@sjsu.edu>
2026-02-21 22:05:50 -0800

    Update MaxHeap.java

--- MaxHeap.java
+++ MaxHeap.java
@@ -10,6 +10,36 @@ public class MaxHeap {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
+    if (left < size && arr[left].frequency > arr[largest].frequency)
+      largest = left;
+
+    if (right < size && arr[right].frequency > arr[largest].frequency)
+      largest = right;
+    
+    if (largest != i){
+      WordFreq temp = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = temp;
+
+      maxHeapify(arr, size, largest);
+    }
+  }
+  public static void main(String[] args) {
+
+    WordFreq [] arr = {
+             new WordFreq("happy", 400),
+                new WordFreq("satisfied", 100),
+                new WordFreq("neutral", 300),
+                new WordFreq("would buy again", 200),
+                new WordFreq("terrible", 160),
+                new WordFreq("inconvenient", 900),
+                new WordFreq("difficult to use", 100),
+                new WordFreq("easy to use", 140),
+                new WordFreq("would recommend to friends", 800),
+                new WordFreq("visit the store", 700)
+    }
+  }
+
     
     
 }
